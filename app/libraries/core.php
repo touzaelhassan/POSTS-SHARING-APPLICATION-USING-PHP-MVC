0b0a6e1cec9c3f4c7c42b94aca22d695910c2ca7
touza <touzaelhassan@gmail.com>
2022-04-21 01:21:50 +0000

    Loading The controller from The URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -9,11 +9,24 @@ class Core
 
   public function __construct()
   {
-    $this->get_url();
+    $url = $this->get_url();
+
+    if (file_exists('../app/controllers' . ucwords($url[0]) . '.php')) {
+      $this->currentController = ucwords($url[0]);
+      unset($url[0]);
+    }
+
+    require_once '../app/controllers/' . $this->currentController . '.php';
+    $this->currentController = new $this->currentController;
   }
 
   public function get_url()
   {
-    echo $_GET['url'];
+    if (isset($_GET["url"])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
