162d6219d152d6f91402342eb68ed4e9ba45698e
touza <touzaelhassan@gmail.com>
2022-04-25 11:59:43 +0000

    Creating Post controller

---
+++ app/views/posts/index.php
@@ -0,0 +1,12 @@
+<?php require APPROOT . "/views/includes/header.php"; ?>
+
+<div class="row">
+  <div class="col-md-6">
+    <h1>Posts</h1>
+  </div>
+  <div class="col-md-6 text-end">
+    <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary pull-right"><i class="fa-solid fa-plus pe-2"></i>ADD</a>
+  </div>
+</div>
+
+<?php require APPROOT . "/views/includes/footer.php"; ?>
